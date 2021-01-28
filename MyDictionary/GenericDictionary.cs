8d723d3ab1e44f118388141f3e2282863f90ea64
aonurd <onurdonmez333@hotmail.com>
2021-01-28 08:19:04 +0300

    Add project files.

---
+++ MyDictionary/GenericDictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+        class MyGenericDictionary<Tkey, TValue>
+        {
+            Tkey[] keyArray;
+            TValue[] valueArray;
+
+            Tkey[] tempKeyArray;
+            TValue[] tempValueArray;
+        public MyGenericDictionary()
+        
+            {
+
+                keyArray = new Tkey[0];
+                valueArray = new TValue[0];
+            
+            }
+
+
+            public void Add(Tkey key, TValue value)
+            {
+                tempKeyArray = keyArray;
+                tempValueArray = valueArray;
+
+                keyArray = new Tkey[keyArray.Length + 1];
+                valueArray = new TValue[valueArray.Length + 1];
+
+                for (int i = 0; i < keyArray.Length - 1; i++)
+                {
+                    keyArray[i] = tempKeyArray[i];
+                    valueArray[i] = tempValueArray[i];
+                }
+
+                keyArray[keyArray.Length - 1] = key;
+                valueArray[valueArray.Length - 1] = value;
+
+            }
+
+            public Tkey[] Items1
+            {
+                get { return keyArray; }
+            }
+
+            public TValue[] Items2
+            {
+                get { return valueArray; }
+            }
+
+            public void Items()
+            {
+                for (int i = 0; i < keyArray.Length; i++)
+                {
+                    Console.WriteLine("Okul Numarası: " + keyArray[i] + " " + "\nÖğrenci İsmi: " + valueArray[i]);
+                    Console.WriteLine("----------------------");
+                }
+            }
+            
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenericDictionary<int, string> dictionary = new MyGenericDictionary<int, string>();
+            dictionary.Add(555, "Abdullah");
+            dictionary.Add(1500, "Onur");
+            dictionary.Add(600, "Ozan");
+
+            dictionary.Items();
+            Console.ReadLine();
+        }
+    }
+}
